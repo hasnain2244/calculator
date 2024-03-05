5f461de252a109acb603cf3c07b84066c8d332c7
hasnain2244 <154471487+hasnain2244@users.noreply.github.com>
2024-03-05 14:39:00 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+/***********************************************/
+/***********************************************/
+/***************Calcuator***********************/
+/***********************************************/
+/***********************************************/
+
+const num1 = +prompt("Calculator \nEnter the number: " );
+
+const num2 = +prompt("Calculator \nEnter the number: " );
+
+let sum = num1 + num2 ;
+
+let sub = num1 - num2 ;
+
+let product = num1 * num2 ;
+
+let divide = num1 / num2 ;
+
+let remainder = num1 % num2 ;
+
+document.write( "<h1>the sum of "+num1+" and "+num2+ " is: ",sum
+      + "<br>the difference of "+num1+" and "+num2+ " is: ",sub
+      + "<br>the product of "+num1+" and "+num2+ " is: ",product
+      + "<br>the quotient of "+num1+" and "+num2+ " is: ",divide
+      + "<br>the remainder of "+num1+" and "+num2+ " is: ",remainder+"</h1>");    
+       
+console.log("the sum of "+num1+" and "+num2+ " is: ",sum
+     + "\nthe difference of "+num1+" and "+num2+ " is: ",sub
+     + "\nthe product of "+num1+" and "+num2+ " is: ",product
+     + "\nthe qoutient of "+num1+" and "+num2+ " is: ",divide
+     + "\nthe remainder of "+num1+" and "+num2+ " is: ",remainder);
+
+
+/****************************************************/
+/*******************The End**************************/
+/****************************************************/
